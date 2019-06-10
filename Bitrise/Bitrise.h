aed36d3cd5343515cdc031fe95279345555509e8
Yu Tawata <yuta24@bivre.com>
2019-06-10 16:36:06 +0900

    Add some frameworks

---
+++ Bitrise/Bitrise.h
@@ -0,0 +1,18 @@
+//
+//  Bitrise.h
+//  Bitrise
+//
+//  Created by tawata-yu on 2019/06/10.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bitrise.
+FOUNDATION_EXPORT double BitriseVersionNumber;
+
+//! Project version string for Bitrise.
+FOUNDATION_EXPORT const unsigned char BitriseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bitrise/PublicHeader.h>
+
+
---
+++ Login/Login.h
@@ -0,0 +1,18 @@
+//
+//  Login.h
+//  Login
+//
+//  Created by tawata-yu on 2019/06/10.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Login.
+FOUNDATION_EXPORT double LoginVersionNumber;
+
+//! Project version string for Login.
+FOUNDATION_EXPORT const unsigned char LoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Login/PublicHeader.h>
+
+
