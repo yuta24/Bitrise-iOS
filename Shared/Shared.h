0fd1489f065626ac23a872bb629d1d1665d762c6
Yu Tawata <yuta24@bivre.com>
2019-06-10 16:03:36 +0900

    Add Shared

---
+++ Shared/Shared.h
@@ -0,0 +1,18 @@
+//
+//  Shared.h
+//  Shared
+//
+//  Created by tawata-yu on 2019/06/10.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shared.
+FOUNDATION_EXPORT double SharedVersionNumber;
+
+//! Project version string for Shared.
+FOUNDATION_EXPORT const unsigned char SharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shared/PublicHeader.h>
+
+
