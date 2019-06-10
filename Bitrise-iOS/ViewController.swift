ea17789b3d9c8d8fbfc3f6e6607a407bcd07d2f9
Yu Tawata <yuta24@bivre.com>
2019-06-10 15:06:31 +0900

    Initialize project

---
+++ Bitrise-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bitrise-iOS
+//
+//  Created by tawata-yu on 2019/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
