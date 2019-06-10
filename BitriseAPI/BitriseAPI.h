f0f3378e934f3ced2a751faee074ac3b20f84528
Yu Tawata <yuta24@bivre.com>
2019-06-10 16:09:36 +0900

    Add Bitrise API

---
+++ BitriseAPI/BitriseAPI.h
@@ -0,0 +1,18 @@
+//
+//  BitriseAPI.h
+//  BitriseAPI
+//
+//  Created by tawata-yu on 2019/06/10.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BitriseAPI.
+FOUNDATION_EXPORT double BitriseAPIVersionNumber;
+
+//! Project version string for BitriseAPI.
+FOUNDATION_EXPORT const unsigned char BitriseAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BitriseAPI/PublicHeader.h>
+
+
